09472ea33606a412ba789d134e8fb87b432882ee
Aishwarya Maheshwari <111682433+aish-w@users.noreply.github.com>
2024-12-08 18:24:51 +0000

    update 2 files done

---
+++ 01_basics/02datatype.js
@@ -0,0 +1,24 @@
+"use strict" //treat all JS code as newer version
+//alert(3+3) //we are using nodejs , not browser
+/*Documentation
+1.tc39-ECMAscript
+2.mdn
+*/
+
+let name="aish";
+let age=21
+let eligible=true;
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>value not asssigned
+//symbol=>unique
+
+console.log(typeof "aish"); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
