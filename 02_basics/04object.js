74dc8eba95f6fa9f1bf17291694710b3652b14f1
Aishwarya Maheshwari <111682433+aish-w@users.noreply.github.com>
2024-12-19 05:07:31 +0000

    object json

--- 02_basics/04object.js
+++ 02_basics/04object.js
@@ -4,7 +4,7 @@ const tinderuser=new Object()
 tinderuser.id="123abc"
 tinderuser.name="aish"
 tinderuser.islogin=false
-console.log(tinderuser); //{ id: '123abc', name: 'aish', islogin: false }
+// console.log(tinderuser); //{ id: '123abc', name: 'aish', islogin: false }
 
 const regularuder={
     email:"aish@gg.com",
@@ -15,13 +15,13 @@ const regularuder={
         }
     }
 }
-console.log(regularuder.fullname.userfullname.firstname); //aishwarya
+// console.log(regularuder.fullname.userfullname.firstname); //aishwarya
 const obj1={1:"a",2:"b"}
 const obj2={3:"a",4:"b"}
 // const obj3={obj1,obj2}
 // console.log(obj3); //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
 const obj3=Object.assign(obj1,obj2)
-console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+// console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
 const users1=[
     {
@@ -38,12 +38,37 @@ const users1=[
     }
 ]
 users1[1].id
-console.log(tinderuser);
-console.log(Object.keys(tinderuser)); //[ 'id', 'name', 'islogin' ]
-console.log(Object.values(tinderuser)); //[ '123abc', 'aish', false ]
-console.log(Object.entries(tinderuser)); //[ [ 'id', '123abc' ], [ 'name', 'aish' ], [ 'islogin', false ] ]
+// console.log(tinderuser);
+// console.log(Object.keys(tinderuser)); //[ 'id', 'name', 'islogin' ]
+// console.log(Object.values(tinderuser)); //[ '123abc', 'aish', false ]
+// console.log(Object.entries(tinderuser)); //[ [ 'id', '123abc' ], [ 'name', 'aish' ], [ 'islogin', false ] ]
 
-console.log(tinderuser.hasOwnProperty('isloging')); //false
+// console.log(tinderuser.hasOwnProperty('isloging')); //false
+
+//object destructuring
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"aish"
+}
+//course.courseInstructor
+// const {courseInstructor}=course
+// console.log(courseInstructor); //aish
+const {courseInstructor:instructor}=course
+console.log(instructor); //aish
+
+//json syntax
+// {
+//     "name":"aish",
+//     "course":"js in hindi",
+//     "price":"free"
+// }
+
+
+
+
+
+
 
 
 
