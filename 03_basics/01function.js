25f2c8103e058591ff0ab447923e4c67557ac2c3
Aishwarya Maheshwari <111682433+aish-w@users.noreply.github.com>
2024-12-20 16:24:37 +0000

    function

---
+++ 03_basics/01function.js
@@ -0,0 +1,33 @@
+//function declaration
+// function saymyname(){
+//     console.log("A");
+//     console.log("i");
+//     console.log("s");
+//     console.log("h");
+// }
+// saymyname()
+
+function addtwonum(num1,num2) {
+    // console.log(num1+num2);
+    
+}
+// addtwonum(3,4) //7
+const result=addtwonum(3,4)
+// console.log(result); //undefined
+function addtwonum(num1,num2) {
+    let result=num1+num2;
+    return result
+}
+// console.log(result);//7
+
+
+function userloginmessage(username){
+    return`${username} just logged in`
+}
+// console.log(userloginmessage("aish")) //aish just logged in
+console.log(userloginmessage()); //undefined just logged in
+
+
+
+
+
