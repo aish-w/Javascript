f02ad44e096b9091232f39cfb168f4becfbe4155
Aishwarya Maheshwari <111682433+aish-w@users.noreply.github.com>
2024-12-20 16:36:14 +0000

    function update

--- 03_basics/01function.js
+++ 03_basics/01function.js
@@ -25,7 +25,25 @@ function userloginmessage(username){
     return`${username} just logged in`
 }
 // console.log(userloginmessage("aish")) //aish just logged in
-console.log(userloginmessage()); //undefined just logged in
+// console.log(userloginmessage()); //undefined just logged in
+
+function calculateprice(...num1){
+    return num1
+}
+console.log(calculateprice(200,400,500)); //[200,400,500]
+
+const user={
+    name:"aish",
+    price:"200"
+}
+function handleobject(any){
+    console.log(`username is ${any.name} and price is ${any.price}`); 
+}
+// handleobject(user) //username is aish and price is 200
+
+
+
+
 
 
 
